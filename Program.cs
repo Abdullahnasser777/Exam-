237221a76a64db1e20719635d045f8bf517f5436
Abdullahnasser777 <abdullahnasser1166@gmail.com>
2026-09-13 22:38:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exam_System_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
